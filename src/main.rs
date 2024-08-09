3c7277037860e9324871685537eeeab825bd756d
Titanium260 <pimkos26@gmail.com>
2024-08-09 19:09:14 +0300

    Made it spit out an html page

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,11 @@ use reqwest;
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     let client = reqwest::Client::new();
-    let res = client.get("https://archlinux.org");
+    let reqwest_builder = reqwest::Client::get(&client, "https://archlinux.org/");
+    let request = reqwest::RequestBuilder::send(reqwest_builder).await?;
+    let res = reqwest::Response::text(request).await?;
+
+    println!("{}", res);
 
     Ok(())
 }
